ee809b4af1324e303c6a3eab3e7c95167f23f44c
ABLippman <lip@media.mit.edu>
2020-10-17 13:17:01 -0400

    Initial Commit

---
+++ DataDetectorIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataDetectorIOS
+//
+//  Created by lip on 10/17/20.
+//  Copyright © 2020 Andrew Lippman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DataDetectorIOS/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DataDetectorIOS
+//
+//  Created by lip on 10/17/20.
+//  Copyright © 2020 Andrew Lippman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
